180baea5ca2bf9d17bd2c69410fe6141dff77b67
angularJS80 <bigcen@naver.com>
2018-12-13 00:01:28 +0900

    jacksonTest

--- src/test/java/cho/me/springwebtest/JsonTest.java
+++ src/test/java/cho/me/springwebtest/JsonTest.java
@@ -1,24 +1,36 @@
 package cho.me.springwebtest;
 
-import com.jayway.jsonpath.internal.JsonContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
+//@org.springframework.boot.test.autoconfigure.json.JsonTest
 public class JsonTest {
-    @Autowired
+    //@Autowired
     JacksonTester<JackTestDto> jacksonTester;
 
+    @Before
+    public void setup(){
+        JacksonTester.initFields(this,new ObjectMapper());
+    }
     @Test
     public void testJson() throws IOException {
         JackTestDto jackTestDto = new JackTestDto();
-        jackTestDto.setName("name");
-        JsonContext jsonContext = json.write(jackTestDto);
-        assertThat(this.json.write(jsonContext).)
+        jackTestDto.setName("cho");
+
+        JsonContent<JackTestDto> jsonContent  = this.jacksonTester.write(jackTestDto);
+        assertThat(jsonContent)
+                .hasJsonPathStringValue("@.name")
+                .extractingJsonPathStringValue("@.name").isEqualTo("cho1");
     }
 }
